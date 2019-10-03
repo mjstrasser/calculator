959085d0ed9614a2597e297907635f842f005681
Michael Strasser <mstrass@thoughtworks.com>
2019-10-03 15:13:08 +1000

    Include keypress in App.

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
+import buttonFromKey from "../logic/keys";
 import "./App.css";
 
 export default class App extends React.Component {
@@ -15,9 +16,13 @@ export default class App extends React.Component {
     this.setState(calculate(this.state, buttonName));
   };
 
+  handleKeypress = key => {
+    this.setState(calculate(this.state, buttonFromKey(key.key)));
+  };
+
   render() {
     return (
-      <div className="component-app">
+      <div className="component-app" onKeyPress={this.handleKeypress}>
         <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
